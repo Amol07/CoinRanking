776de6413c5ea54deacd615bb05de19e6d66cd21
Amol Prakash <amolprakash06@gmail.com>
2025-01-21 04:29:06 +0530

    refactoring

--- CoinRanking/Features/CoinFilter/View/FilterViewController.swift
+++ CoinRanking/Features/CoinFilter/View/FilterViewController.swift
@@ -15,80 +15,78 @@ class FilterViewController: UIViewController {
     private var selectedFilter: FilterOption?
     private var selectedOrder: OrderOption?
 
-    // MARK: - Initialiser
-    init(viewModel: FilterViewModel) {
-        self.viewModel = viewModel
-        super.init(nibName: nil, bundle: nil)
-        self.selectedFilter = viewModel.selectedFilter
-        self.selectedOrder = viewModel.selectedOrder
-    }
+    // MARK: - Private views
 
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-
-    // MARK: - Lifecycle methods
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = UIColor.black.withAlphaComponent(0.5)
-        setupDrawerView()
-    }
-
-    // MARK: - Private methods
-    private func setupDrawerView() {
-        let containerView = UIView()
-        containerView.backgroundColor = .white
-        containerView.layer.cornerRadius = 16
-        containerView.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(containerView)
-
-        NSLayoutConstraint.activate([
-            containerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            containerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            containerView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-            containerView.heightAnchor.constraint(equalToConstant: 300)
-        ])
-
+    // UILabel for displaying the "Filter By" label
+    private lazy var filterLabel: UILabel = {
         let filterLabel = UILabel()
         filterLabel.text = "Filter By"
         filterLabel.font = .boldSystemFont(ofSize: 18)
         filterLabel.translatesAutoresizingMaskIntoConstraints = false
-        containerView.addSubview(filterLabel)
+        return filterLabel
+    }()
 
+    // UISegmentedControl for selecting a filter option
+    private lazy var filterSegmentedControl: UISegmentedControl = {
         let filterSegmentedControl = UISegmentedControl(items: self.viewModel.filterOptions.map { $0.textValue })
         filterSegmentedControl.addTarget(self, action: #selector(filterChanged(_:)), for: .valueChanged)
         filterSegmentedControl.translatesAutoresizingMaskIntoConstraints = false
-        containerView.addSubview(filterSegmentedControl)
-
         if let selectedFilter = viewModel.selectedFilter, let index = self.viewModel.filterOptions.firstIndex(of: selectedFilter) {
             filterSegmentedControl.selectedSegmentIndex = index
         }
+        return filterSegmentedControl
+    }()
 
+    // UILabel for displaying the "Order" label
+    private lazy var orderLabel: UILabel = {
         let orderLabel = UILabel()
         orderLabel.text = "Order"
         orderLabel.font = .boldSystemFont(ofSize: 18)
         orderLabel.translatesAutoresizingMaskIntoConstraints = false
-        containerView.addSubview(orderLabel)
+        return orderLabel
+    }()
 
-        let orderSegmentedControl = UISegmentedControl(items: self.viewModel.orderOptions.map { $0.textValue })
+    // UISegmentedControl for selecting an order option
+    private lazy var orderSegmentedControl: UISegmentedControl = {
+        let orderSegmentedControl = UISegmentedControl(items: self.viewModel.orderOptions.map{ $0.textValue })
         orderSegmentedControl.addTarget(self, action: #selector(orderChanged(_:)), for: .valueChanged)
         orderSegmentedControl.translatesAutoresizingMaskIntoConstraints = false
-        containerView.addSubview(orderSegmentedControl)
-
         if let selectedOrder = viewModel.selectedOrder, let index = self.viewModel.orderOptions.firstIndex(of: selectedOrder) {
             orderSegmentedControl.selectedSegmentIndex = index
         }
+        return orderSegmentedControl
+    }()
 
+    // UIButton for saving the selected filters
+    private lazy var saveButton: UIButton = {
         let saveButton = UIButton(type: .system)
         saveButton.setTitle("Save", for: .normal)
         saveButton.addTarget(self, action: #selector(saveFilters), for: .touchUpInside)
         saveButton.translatesAutoresizingMaskIntoConstraints = false
-        containerView.addSubview(saveButton)
+        return saveButton
+    }()
 
+    // UIButton for resetting the filters to default values
+    private lazy var resetButton: UIButton = {
         let resetButton = UIButton(type: .system)
         resetButton.setTitle("Reset", for: .normal)
         resetButton.addTarget(self, action: #selector(resetFilters), for: .touchUpInside)
         resetButton.translatesAutoresizingMaskIntoConstraints = false
+        return resetButton
+    }()
+
+    // Container view to hold all the UI components
+    private lazy var containerView: UIView = {
+        let containerView = UIView()
+        containerView.backgroundColor = .white
+        containerView.layer.cornerRadius = 16
+        containerView.translatesAutoresizingMaskIntoConstraints = false
+
+        containerView.addSubview(filterLabel)
+        containerView.addSubview(filterSegmentedControl)
+        containerView.addSubview(orderLabel)
+        containerView.addSubview(orderSegmentedControl)
+        containerView.addSubview(saveButton)
         containerView.addSubview(resetButton)
 
         NSLayoutConstraint.activate([
@@ -111,6 +109,38 @@ class FilterViewController: UIViewController {
 
             resetButton.topAnchor.constraint(equalTo: orderSegmentedControl.bottomAnchor, constant: 16),
             resetButton.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -16)
+        ])
+        return containerView
+    }()
+
+    // MARK: - Initialiser
+    init(viewModel: FilterViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+        self.selectedFilter = viewModel.selectedFilter
+        self.selectedOrder = viewModel.selectedOrder
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - Lifecycle methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.black.withAlphaComponent(0.5)
+        setupDrawerView()
+    }
+
+    // MARK: - Private methods
+    private func setupDrawerView() {
+        view.addSubview(containerView)
+
+        NSLayoutConstraint.activate([
+            containerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            containerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            containerView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            containerView.heightAnchor.constraint(equalToConstant: 300)
         ])
     }
 
